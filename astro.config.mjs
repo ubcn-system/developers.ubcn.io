b74dc007d64567b1449a72bdf03e10788e2bae0c
taewoo <twkim326@gmail.com>
2024-07-19 10:20:37 +0900

    feat($browser): add badge span

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: "https://ubcn.co.kr",
+  compressHTML: false,
   integrations: [
     starlight({
       title: '개발자센터',
